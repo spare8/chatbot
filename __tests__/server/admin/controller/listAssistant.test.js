ff51645c34fa3fdffad396e4f23ad641030c8a02
Shardul Saiya <shardul@spare8.com>
2025-08-01 17:11:33 +0530

    Add tests for assistants API

---
+++ __tests__/server/admin/controller/listAssistant.test.js
@@ -0,0 +1,29 @@
+const {listAssistants} = require('../../../../server/admin/controller/listAssistants');
+const {getAllAssistants} = require('../../../../server/admin/dbInteractions');
+const {MockResponse} = require('../../../setupTests');
+
+jest.mock('../../../../server/admin/dbInteractions', () => ({
+    getAllAssistants: jest.fn()
+}));
+
+const mockAssistants = [
+      {id: '1', name: 'Assistant 1'},
+      {id: '2', name: 'Assistant 2'},
+    ];
+describe('listAssistants', () => {
+  let res, req;
+
+  beforeAll( ()=> {
+    getAllAssistants.mockResolvedValue(mockAssistants);
+  });
+  beforeEach(() => {
+    res = new MockResponse();
+    req = {};
+  });
+
+  it('should return a list of assistants', async () => {
+    await listAssistants(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockAssistants);
+  });
+});
