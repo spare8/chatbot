66ff9d4b8ba64547cc5db981b24f4f06246b3dd4
Shardul Saiya <shardul@spare8.com>
2025-08-05 14:48:59 +0530

    clean up assistants model

--- models/assistant.js
+++ models/assistant.js
@@ -3,55 +3,6 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 const {SUPPORTED_MODELS, DEFAULT_MODEL} = require('../config/constants.js');
 
-// const assistantSchema = new Schema(
-//     {
-//       name: {
-//         type: String,
-//         required: true,
-//       },
-//       openaiId: {
-//         type: String,
-//         required: true,
-//       },
-//       description: {
-//         type: String,
-//         default: '',
-//       },
-//       instructions: {
-//         type: String,
-//         default: '',
-//       },
-//       model: {
-//         type: String,
-//         required: true,
-//         enum: Object.values(SUPPORTED_MODELS),
-//         default: DEFAULT_MODEL,
-//       },
-//       temperature: {
-//         type: Number,
-//         default: 0.7,
-//       },
-//       vectorStoreId: {
-//         type: String,
-//       },
-//       isDeleted: {
-//         type: Boolean,
-//       },
-//     },
-//     {
-//       timestamps: true,
-//     },
-// );
-
-// const Assistants = mongoose.model('Assistant', assistantSchema);
-// module.exports = Assistants;
-
-
-// // server/models/assistant.js
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-// const { SUPPORTED_MODELS, DEFAULT_MODEL } = require('../config/constants.js');
-
 // Sub-schema for tools configuration
 const toolSchema = new Schema({
   type: {
@@ -121,5 +72,6 @@ const assistantSchema = new Schema({
   timestamps: true,
 });
 
-module.exports = mongoose.model('Assistant', assistantSchema);
+const Assistants = mongoose.model('Assistant', assistantSchema);
+module.exports = Assistants;
 
